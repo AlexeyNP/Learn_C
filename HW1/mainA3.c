38e41efc36b45d378925ee3cac30a1c66d72518b
Alexey Petukhov <emperon@mail.ru>
2025-02-08 16:51:47 +0300

    2

---
+++ HW1/mainA3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    int d=a+b+c;
+    printf("%d+%d+%d=%d", a, b, c, d);
+    return 0;
+}
